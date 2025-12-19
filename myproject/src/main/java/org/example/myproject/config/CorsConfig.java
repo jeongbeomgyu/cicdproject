988c13afbb861fe47f86775905df157090cd6132
jeongbeomgyu <jeongbeomgyu@github.com>
2025-12-19 13:54:38 +0900

    fix:allowed all origin

--- myproject/src/main/java/org/example/myproject/config/CorsConfig.java
+++ myproject/src/main/java/org/example/myproject/config/CorsConfig.java
@@ -13,6 +13,9 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+
+        // 모든 오리진 허용 
+        config.addAllowedOriginPattern("*");
         
         // 프론트엔드 포트 허용 (Next.js 기본 포트: 3000)
         config.addAllowedOrigin("http://localhost:3000");
